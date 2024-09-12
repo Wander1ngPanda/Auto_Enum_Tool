f0e2cbc36a67f3e73032c8bdb73cb14746188852
Wander1ngPanda <jack@ukmurphy.com>
2024-09-12 20:43:07 +0100

    Quick fix

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ ip, port, verbose, flag, wordlist = args().getArgs()
 
 if port == "80":
     url = f"http://{ip}:{port}/FUZZ.html"
-    procedure = subprocess.Popen(['ffuf', '-w', wordlist + ':FUZZ', '-u', url, '-ic'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = f"ffuf -w {wordlist}:FUZZ -u {url} -ic"
+    procedure = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = procedure.communicate()
     print(output)
 
